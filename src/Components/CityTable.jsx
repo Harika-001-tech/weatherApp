66198dcfac28e5f279fa57ab3cc22733a72de159
Harika-001-tech <157860376+Harika-001-tech@users.noreply.github.com>
2024-09-08 11:38:35 +0530

    deploy

--- src/Components/CityTable.jsx
+++ src/Components/CityTable.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { BiSearch,BiCurrentLocation } from "react-icons/bi";
 
+//city table
 const CityTable = () => {
   const [cities, setCities] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
